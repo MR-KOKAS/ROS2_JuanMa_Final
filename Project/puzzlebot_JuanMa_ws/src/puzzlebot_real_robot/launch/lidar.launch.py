b52bbaa98e78eba4789f9d590842024a73bef2f0
Itzel-hdz <a01709672@tec.mx>
2026-05-20 11:15:24 -0600

    Carpetas que faltaban

---
+++ Project/puzzlebot_JuanMa_ws/src/puzzlebot_real_robot/launch/lidar.launch.py
@@ -0,0 +1,38 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    # Driver del RPLiDAR A1
+    rplidar_driver = Node(
+       package='sllidar_ros2',
+       executable='sllidar_node',
+       name='sllidar_node',
+       output='screen',
+       parameters=[{
+          'channel_type': 'serial',
+          'serial_port': '/dev/ttyUSB1',
+          'serial_baudrate': 115200,
+          'frame_id': 'rplidar_link',
+          'inverted': False,
+          'angle_compensate': True,
+          'scan_mode': 'Standard',
+          }],
+      )
+  
+    lidar_tf = Node(
+        package='tf2_ros',
+        executable='static_transform_publisher',
+        name='lidar_static_tf',
+        output='screen',
+        arguments=[
+           '--x', '0', '--y', '0', '--z', '0',
+           '--yaw', '3.14159', '--pitch', '0', '--roll', '0',
+           '--frame-id', 'laser_frame',
+           '--child-frame-id', 'rplidar_link',
+           ],
+        )
+    
+    return LaunchDescription([
+        rplidar_driver,
+        lidar_tf,
+        ])
